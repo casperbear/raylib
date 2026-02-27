d3a9c4eb450a50c7347d9db4b1c91ee83527cbec
casperbear <casperbear@yandex.ru>
2026-02-27 13:14:27 +0500

    update

--- src/config.h
+++ src/config.h
@@ -178,10 +178,6 @@
 //#define RL_CULL_DISTANCE_NEAR                  0.125   // Default projection matrix near cull distance
 //#define RL_CULL_DISTANCE_FAR                2000.0    // Default projection matrix far cull distance
 
-#define RL_MAX_MATRIX_STACK_SIZE              32      // Maximum size of internal Matrix stack
-
-#define RL_MAX_SHADER_LOCATIONS               32      // Maximum number of shader locations supported
-
 #define RL_CULL_DISTANCE_NEAR                  0.125   // Default projection matrix near cull distance
 #define RL_CULL_DISTANCE_FAR                2000.0    // Default projection matrix far cull distance
 
